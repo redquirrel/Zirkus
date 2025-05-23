59a0bd7434b860dbfd212d698d8168df82d3b221
redquirrel <sofie.lorenz@posteo.de>
2025-05-23 10:56:26 +0200

    Update Hero.tsx
    -juggling ball downwards, blurred background less obvious

--- circus-for-everyone-main/src/components/Hero.tsx
+++ circus-for-everyone-main/src/components/Hero.tsx
@@ -28,35 +28,38 @@ export default function Hero() {
           </div>
       
           {/* Overlay Content */}
-          <div className="absolute top-28 md:top-20 lg:top-20 left-1/2 transform -translate-x-1/2 md:py-20 h-full flex flex-col items-center text-center">
-            <div className="text-4xl md:text-6xl lg:text-7xl font-bold leading-tight">
-              <div className="backdrop-blur-sm">
-                <motion.h1 
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                >
-                  Bewegt.Sein
-                </motion.h1>
-              </div>
-            </div>
-            <motion.div
+          <div className="absolute top-28 md:top-20 lg:top-20 left-1/2 transform -translate-x-1/2 md:py-20 md:pt-8 h-full flex flex-col items-center text-center">
+        <div className="text-4xl md:text-6xl lg:text-7xl lg:mt-2 md:mt-[-4] font-bold leading-tight">
+          <div className="backdrop-blur-sm">
+            <motion.h1
               initial={{ opacity: 0, y: 100 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.1 }}
             >
-              <p className="font-semibold text-lg md:text-2xl lg:text-3xl text-white mb-8 mt-2 md:mt-3 lg:mt-4 md:pb-6 backdrop-blur-sm">
-                Inklusive Kurse für Tanz <br />
-                und Bewegungskünste
-              </p>
+              Bewegt.Sein
+            </motion.h1>
+          </div>
+        </div>
+            <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.1 }}
+        >
+          <p
+            className="font-semibold text-lg md:text-2xl lg:text-3xl text-white mb-8 md:mb-36 lg:mb-64 xl:mb-8 mt-2 md:mt-3 
+          lg:mt-4 md:pb-6 backdrop-blur-sm md:backdrop-blur-sm lg:backdrop-blur-none"
+          >
+            Inklusive Kurse für Tanz <br />
+            und Bewegungskünste
+          </p>
 
 
-              <Link
-                href="#aktuell"
-                className="inline-block px-6 py-3 md:px-7 md:py-4 lg:px-10 lg:py-6 text-lg font-semibold text-white hover:text-contrast rounded-full 
+               <Link
+            href="#aktuell"
+            className="inline-block px-6 py-3 md:px-7 md:py-4 lg:px-10 lg:py-6 text-lg font-semibold text-white hover:text-contrast rounded-full 
                 bg-gradient-to-tr from-red to-darkbg hover:bg-white border border-gold transition duration-300 hover:scale-110"
-              >
-                Aktuelle Kurse
-              </Link>
+          >
+            Aktuelle Kurse
+          </Link>
 
 
 
